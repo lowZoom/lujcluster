f03287f1c10b6f47b07026f08e5cd950e1129247
luj <david_lu_st@163.com>
2020-03-09 17:38:51 +0800

    整理代码

--- lujcluster/src/main/java/luj/cluster/internal/node/message/listener/NodeMessageListenInvokerImplV2.java
+++ lujcluster/src/main/java/luj/cluster/internal/node/message/listener/NodeMessageListenInvokerImplV2.java
@@ -22,7 +22,7 @@ final class NodeMessageListenInvokerImplV2 implements NodeMessageListenInvoker {
   public void invoke() {
     Collection<Class<?>> actorTypes = _handleMap.getHandleList(_messageKey);
     if (actorTypes.isEmpty()) {
-      LOG.debug("消息没有处理器，忽略：{}", _messageKey);
+      LOG.info("消息没有处理器，忽略：{}", _messageKey);
       return;
     }
 
